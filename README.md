0d33825c28b12640fbbe2f7408e2c1303ded4f31
Erik Norgren <erik.norgren@interactivesolutions.se>
2017-12-20 14:36:41 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+# ElasticSearch Hook for [Logrus](https://github.com/Sirupsen/logrus)
+Features:
+- Asynchronous
+- Updates elastic search in bulks, reducing strain on the ElasticSearch server
+
+## Installation
+`go get github.com/interactive-solutions/go-logrus-elasticsearch`
+`glide get github.com/interactive-solutions/go-logrus-elasticsearch`
+
+## Usage
+```go
+package main
+    
+import (
+    "os"
+
+    "strings"
+
+    "fmt"
+    "time"
+)
+    
+func main() {
+    // Create logger
+    logger := logrus.New()
+    
+    // Create elastic client
+    client, err := elastic.NewClient(elastic.SetURL("localhost:9200"))
+    if err != nil {
+        logger.WithError(err).Fatal("Failed to construct elasticsearch client")
+    }
+    
+    // Create logger with 15 seconds flush interval
+    hook, err := logging.NewElasticHook(client, "some-host", logrus.DebugLevel, func() string {
+        return fmt.Sprintf("%s-%s", "some-index", time.Now().Format("2006-01-02"))
+    }, time.Second * 15)
+    
+    if err != nil {
+        logger.WithError(err).Fatal("Failed to create elasticsearch hook for logger")
+    }
+    
+    logger.Hooks.Add(hook)
+    logger.Info("All done")
+}
+```
---
+++ hook.go
@@ -0,0 +1,132 @@
+package go_logrus_elasticsearch
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+	"gopkg.in/olivere/elastic.v5"
+)
+
+var (
+	// Fired if the
+	// index is not created
+	ErrCannotCreateIndex = fmt.Errorf("Cannot create index")
+)
+
+const FlushInterval = time.Second * 15
+
+type IndexNameFunc func() string
+
+type ElasticSearchHook struct {
+	processor *elastic.BulkProcessor
+	host      string
+	index     IndexNameFunc
+	levels    []logrus.Level
+	ctx       context.Context
+	ctxCancel context.CancelFunc
+}
+
+func NewElasticHook(
+	client *elastic.Client,
+	host string,
+	level logrus.Level,
+	indexFunc IndexNameFunc,
+	flushInterval time.Duration,
+) (*ElasticSearchHook, error) {
+
+	levels := []logrus.Level{}
+	for _, l := range []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+		logrus.DebugLevel,
+	} {
+		if l <= level {
+			levels = append(levels, l)
+		}
+	}
+
+	ctx, cancel := context.WithCancel(context.TODO())
+
+	exists, err := client.IndexExists(indexFunc()).Do(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if !exists {
+		createIndex, err := client.CreateIndex(indexFunc()).Do(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if !createIndex.Acknowledged {
+			return nil, ErrCannotCreateIndex
+		}
+	}
+
+	// from elastic docs: "If you want the bulk processor to
+	// operate completely asynchronously, set both BulkActions and BulkSize to
+	// -1 and set the FlushInterval to a meaningful flushInterval."
+	processor, err := client.BulkProcessor().
+		Name(host).
+		BulkActions(-1).
+		BulkSize(-1).
+		FlushInterval(flushInterval).
+		Do(ctx)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to create bulk processor")
+	}
+
+	return &ElasticSearchHook{
+		processor: processor,
+		index:     indexFunc,
+		host:      host,
+		levels:    levels,
+		ctx:       ctx,
+		ctxCancel: cancel,
+	}, nil
+}
+
+func (hook *ElasticSearchHook) Fire(entry *logrus.Entry) error {
+	level := entry.Level.String()
+
+	if e, ok := entry.Data[logrus.ErrorKey]; ok && e != nil {
+		if err, ok := e.(error); ok {
+			entry.Data[logrus.ErrorKey] = err.Error()
+		}
+	}
+
+	msg := struct {
+		Host      string
+		Timestamp string `json:"@timestamp"`
+		Message   string
+		Data      logrus.Fields
+		Level     string
+	}{
+		hook.host,
+		entry.Time.UTC().Format(time.RFC3339Nano),
+		entry.Message,
+		entry.Data,
+		strings.ToUpper(level),
+	}
+
+	r := elastic.NewBulkIndexRequest().Index(hook.index()).Type("log").Doc(msg)
+	hook.processor.Add(r)
+
+	return nil
+}
+
+func (hook *ElasticSearchHook) Levels() []logrus.Level {
+	return hook.levels
+}
+
+func (hook *ElasticSearchHook) Cancel() {
+	hook.ctxCancel()
+}
